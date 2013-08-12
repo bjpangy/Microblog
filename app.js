908f45c76e7b84988dfd006cba7fa0c38c1a86d2
bjpangy <bjpangy@gmail.com>
2013-08-12 17:43:13 +0800

    08212013

--- app.js
+++ app.js
@@ -7,26 +7,68 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
-
+  , path = require('path')
+  , partials = require('express-partials')
+  , MongoStore = require('connect-mongo')(express)
+  , settings = require('./settings')
+  , flash = require('./node_modules/flash.js');
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function(){
+    // all environments
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(partials());
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session(
+        {secret:settings.cookieSecret,
+         store:new MongoStore({db:settings.db})
+        }));
+    //app.use(express.router(routes));
+    app.use(flash());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 
+    app.use(function(req,res,next){
+        res.locals.user = req.session.user;
+        next();
+    }
+    );
+});
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+routes(app);
+
+/*app.dynamicHelpers({
+    user:function(req,res){
+        return req.sesstion.user;
+    },
+    error:function(req,res){
+        var err = req.flash('error');
+        if(err.length){
+            return err;
+        }else{
+            return null;
+        }
+    },
+    success:function(req,res){
+        var succ = req.flash('success');
+        if(succ.length){
+            return success;
+        }else{
+            return null;
+        }
+    }});
+*/
+/*
 app.get('/', routes.index);
 app.get('/u/:user',routes.user );
 app.post('/post',routes.post);
@@ -35,6 +77,7 @@ app.post('/reg',routes.doReg);
 app.get('/login',routes.login);
 app.post('/login',routes.doLogin);
 app.get('/logout',routes.logout);
+*/
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
  * GET home page.
  */
 
+/*
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
@@ -22,3 +23,57 @@ exports.doLogin= function(req,res){
 };
 exports.logout= function(req,res){
 };
+*/
+
+var crypto = require('crypto');
+var User = require('../model/user.js');
+
+module.exports = function(app){
+    app.get('/',function(req,res){
+        res.render('index',{title:'Home'});
+    });
+
+    app.get('/reg',function(req,res){
+        res.render('reg',{title:'Reg'})
+    });
+
+    app.post('/reg',function(req,res){
+       console.log('doreg');
+        //check password same
+        if(req.body['password-repeat'] != req.body['password']){
+            req.flash('error','password should same');
+            return res.redirect('/reg');
+        }
+
+        //crypto password
+        var md5 = crypto.createHash('md5');
+        var password = md5.update(req.body.password).digest('base64');
+
+        var newUser = new User({
+            name: req.body.username,
+            password:password,
+        });
+
+        //check user exists
+        User.get(newUser.name,function(err,user){
+            if(user)
+                err = 'username already exists';
+            if (err){
+                req.flash('error',err);
+                return res.redirect('/reg');
+            }
+        
+        //add user
+        newUser.save(function (err){
+            if (err){
+                req.flash('error',err);
+                return res.redirect('/reg');
+            }
+            req.session.user = newUser;
+            req.flash('success','registered successfully');
+            res.redirect('/');
+        });
+      })
+     }//post callback fun
+    );//post
+};//exports
